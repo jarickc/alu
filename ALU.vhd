6861e3461a93872bc4c9dff405cf24cf968ad334
capalongodillon <55099084+capalongodillon@users.noreply.github.com>
2019-11-13 18:41:45 -0500

    Update ALU.vhd

--- ALU.vhd
+++ ALU.vhd
@@ -3,12 +3,14 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_signed.all;
 -- Attempt to make a truth table for an AND gate!
 entity ALU_VHDL is
+
 port(
-a,b : in std_logic_vector; -- 2 operands, 1 bit each for AND gate
-result : out std_logic_vector -- Output is 1 bit
+operand1 : in std_logic;
+operand2 : in std_logic; -- 2 operands, 1 bit each for AND gate
+result : out std_logic -- Output is 1 bit
 );
 end ALU_VHDL;
 architecture Maths of ALU_VHDL is
 begin
-result <= a and b;
+result <= operand1 and operand2;
 end Maths; 

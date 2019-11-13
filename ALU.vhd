92e1bab79d10d79c359bf97deb7e71cd66d30ec9
capalongodillon <55099084+capalongodillon@users.noreply.github.com>
2019-11-13 12:52:24 -0500

    Corrected Syntax
    I had multiple syntax errors that I fixed.

--- ALU.vhd
+++ ALU.vhd
@@ -5,10 +5,10 @@ use IEEE.STD_LOGIC_signed.all;
 entity ALU_VHDL is
 port(
 a,b : in std_logic_vector; -- 2 operands, 1 bit each for AND gate
-result : out std_logic_vector; -- Output is 1 bit
+result : out std_logic_vector -- Output is 1 bit
 );
 end ALU_VHDL;
-
-Architecture Maths of ALU is
+architecture Maths of ALU_VHDL is
+begin
 result <= a and b;
 end Maths; 
